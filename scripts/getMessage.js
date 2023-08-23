a8f67c3e6d2d6d1025ae0700ad486a10ab865bf6
ChainZoku <138441009+chainzoku@users.noreply.github.com>
2023-08-23 16:40:01 +0000

    initial assets

---
+++ scripts/getMessage.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/swisstronik.js");
+
+const sendShieldedQuery = async (provider, destination, data) => {
+  const rpclink = hre.network.config.url;
+  const [encryptedData, usedEncryptedKey] = await encryptDataField(rpclink, data);
+  const response = await provider.call({
+    to: destination,
+    data: encryptedData,
+  });
+  return await decryptNodeResponse(rpclink, response, usedEncryptedKey);
+};
+
+async function main() {
+  const contractAddress = "0x2636E59E51F95176773A6243216F729c9F8C6292";
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "getMessage";
+  const responseMessage = await sendShieldedQuery(signer.provider, contractAddress, contract.interface.encodeFunctionData(functionName));
+  console.log("Decoded response:", contract.interface.decodeFunctionResult(functionName, responseMessage)[0]);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
